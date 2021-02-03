d36b9a19ff9075332ace349678236d527cd1d2c9
francisLiu <975924594@qq.com>
2021-02-03 12:43:33 +0800

    2021-02-03Exer

--- src/com/FrancisExer_03_TicketSeller/TicketSellerDemo.java
+++ src/com/FrancisExer_03_TicketSeller/TicketSellerDemo.java
@@ -3,8 +3,8 @@ package com.FrancisExer_03_TicketSeller;
 public class TicketSellerDemo {
     public static void main(String[] args) {
 
-        TicketSeller ts=new TicketSeller();
-
+        //TicketSellerSynchronizedDemo ts=new TicketSellerSynchronizedDemo();
+        TicketSellerLockDemo ts=new TicketSellerLockDemo();
         Thread t1=new Thread(ts,"窗口1");
         Thread t2=new Thread(ts,"窗口2");
         Thread t3=new Thread(ts,"窗口3");
